812c99aaf2b61091e89bc725daded216af1de978
Mohsin Murtuza <mohsinmurtuza@Mohsins-MacBook-Air.local>
2026-02-04 22:08:16 +0000

    first spring ai app

---
+++ first-ai-app/src/test/java/com/systa/FirstAiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.systa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
