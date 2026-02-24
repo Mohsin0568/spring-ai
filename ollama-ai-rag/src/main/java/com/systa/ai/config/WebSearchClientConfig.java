a30dfc751485e6259785c4d1a0e45876c866b0ac
Mohsin Murtuza <mohsinmurtuza@Mohsins-MacBook-Air.local>
2026-02-24 22:07:09 +0000

    added pre and post processor for documents before and after sending to llm

--- ollama-ai-rag/src/main/java/com/systa/ai/config/WebSearchClientConfig.java
+++ ollama-ai-rag/src/main/java/com/systa/ai/config/WebSearchClientConfig.java
@@ -1,6 +1,7 @@
 package com.systa.ai.config;
 
 import com.systa.ai.advisors.TokenUsageAuditAdvisor;
+import com.systa.ai.rag.PIIMaskingDocumentPostProcessor;
 import com.systa.ai.rag.WebSearchDocumentRetriever;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
@@ -8,6 +9,7 @@ import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.preretrieval.query.transformation.TranslationQueryTransformer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -18,14 +20,21 @@ import java.util.List;
 public class WebSearchClientConfig {
 
     @Bean("webSearchRAGChatClient")
-    public ChatClient chatClient(ChatClient.Builder chatClientBuilder,
-                                 ChatMemory chatMemory, RestClient.Builder restClientBuilder) {
-        Advisor loggerAdvisor = new SimpleLoggerAdvisor();
-        Advisor tokenUsageAdvisor = new TokenUsageAuditAdvisor();
-        Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
-        var webSearchRAGAdvisor = RetrievalAugmentationAdvisor.builder()
+    public ChatClient chatClient(final ChatClient.Builder chatClientBuilder,
+                                 final ChatMemory chatMemory, final RestClient.Builder restClientBuilder) {
+        final Advisor loggerAdvisor = new SimpleLoggerAdvisor();
+        final Advisor tokenUsageAdvisor = new TokenUsageAuditAdvisor();
+        final Advisor memoryAdvisor = MessageChatMemoryAdvisor.builder(chatMemory).build();
+        final var webSearchRAGAdvisor = RetrievalAugmentationAdvisor.builder()
+                .queryTransformers(TranslationQueryTransformer
+                        .builder()
+                        .chatClientBuilder(chatClientBuilder.clone())
+                        .targetLanguage("english")
+                        .build()
+                )
                 .documentRetriever(WebSearchDocumentRetriever.builder()
                         .restClientBuilder(restClientBuilder).maxResults(5).build())
+                .documentPostProcessors(PIIMaskingDocumentPostProcessor.builder())
                 .build();
         return chatClientBuilder
                 .defaultAdvisors(List.of(loggerAdvisor, memoryAdvisor, tokenUsageAdvisor,
